b98f20e4b3585636ac1223ad2b1c12409677d836
asif-iqbal-munna <asifiqbalmunna1318@gmail.com>
2021-12-28 12:59:50 +0600

    practices nest with typescript

---
+++ src/courses/create-course.dto.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+export class CreateCourseDto {
+    readonly id: number;
+    readonly title: string;
+    readonly description: string;
+    readonly author: string;
+    readonly url: string;
+}
